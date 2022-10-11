a7f1002e5bae100797a4d1cab0f9eea4d8ee754a
msorokovikov <max71211@yandex.ru>
2022-10-11 23:29:02 +0300

    Car serie | Car generation

--- datastore/auto_mysql/car_modification.go
+++ datastore/auto_mysql/car_modification.go
@@ -3,14 +3,19 @@ package auto_mysql
 import (
 	"aopt-db-sync/models"
 	"context"
+	"database/sql"
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	"time"
 )
 
 const (
-	carModificationFields = "id_car_modification, id_car_serie, id_car_model, name, start_production_year, end_production_year, date_create, date_update, id_car_type"
+	carModificationFields = `id_car_modification, id_car_serie, id_car_model, car_modification.name, start_production_year, end_production_year, date_create, date_update, id_car_type`
 	carModificationTable  = "car_modification"
+	carSerieFields        = `car_serie.name as serie_name`
+	carSerieTable         = "car_serie"
+	carGenerationFields   = `car_generation.name as generation_name, car_generation.year_begin as generation_year_begin, car_generation.year_end as generation_year_end`
+	carGenerationTable    = "car_generation"
 )
 
 // NewCarModificationRepository create data access layer CarModificationRepository
@@ -24,15 +29,19 @@ type CarModificationRepository struct {
 }
 
 type carModificationDTO struct {
-	ID                  int    `db:"id_car_modification"`
-	IDCarSerie          int    `db:"id_car_serie"`
-	IDCarModel          int    `db:"id_car_model"`
-	Name                string `db:"name"`
-	StartProductionYear *int   `db:"start_production_year"`
-	EndProductionYear   *int   `db:"end_production_year"`
-	DateCreate          int64  `db:"date_create"`
-	DateUpdate          int64  `db:"date_update"`
-	IDCarType           int    `db:"id_car_type"`
+	ID                  int            `db:"id_car_modification"`
+	IDCarSerie          int            `db:"id_car_serie"`
+	IDCarModel          int            `db:"id_car_model"`
+	Name                string         `db:"name"`
+	StartProductionYear *int           `db:"start_production_year"`
+	EndProductionYear   *int           `db:"end_production_year"`
+	SerieName           string         `db:"serie_name"`
+	GenerationName      string         `db:"generation_name"`
+	GenerationYearBegin string         `db:"generation_year_begin"`
+	GenerationYearEnd   sql.NullString `db:"generation_year_end"`
+	DateCreate          int64          `db:"date_create"`
+	DateUpdate          int64          `db:"date_update"`
+	IDCarType           int            `db:"id_car_type"`
 }
 
 func (dto *carModificationDTO) Entity() *models.CarModification {
@@ -70,7 +79,10 @@ func (repo *CarModificationRepository) GetAll(ctx context.Context) ([]*models.Ca
 func (repo *CarModificationRepository) GetByCarModelID(ctx context.Context, carModelID int) ([]*models.CarModification, error) {
 	var dtos []*carModificationDTO
 	err := sqlx.SelectContext(ctx, repo.db, &dtos,
-		fmt.Sprintf(`SELECT %s FROM %s WHERE id_car_model = %d;`, carModificationFields, carModificationTable, carModelID))
+		fmt.Sprintf(`SELECT %s FROM %s 
+JOIN %s ON %[1]s.id_car_serie = %[2]s.id_car_serie
+JOIN %s ON %[2]s.id_car_generation = %[3]s.id_car_generation
+WHERE id_car_model = %d;`, fmt.Sprintf("%s, %s, %s", carModificationFields, carSerieFields, carGenerationFields), carModificationTable, carSerieTable, carGenerationTable, carModelID))
 	if err != nil {
 		return nil, err
 	}
--- models/auto.go
+++ models/auto.go
@@ -23,13 +23,23 @@ type CarModel struct {
 }
 
 type CarModification struct {
-	ID                  int       `json:"id_car_modification"`
-	IDCarSerie          int       `json:"id_car_serie"`
-	IDCarModel          int       `json:"id_car_model"`
-	Name                string    `json:"name"`
-	StartProductionYear *int      `json:"start_production_year"`
-	EndProductionYear   *int      `json:"end_production_year"`
-	DateCreate          time.Time `json:"date_create"`
-	DateUpdate          time.Time `json:"date_update"`
-	IDCarType           int       `json:"id_car_type"`
+	ID                  int           `json:"id_car_modification"`
+	IDCarSerie          int           `json:"id_car_serie"`
+	IDCarModel          int           `json:"id_car_model"`
+	Name                string        `json:"name"`
+	StartProductionYear *int          `json:"start_production_year"`
+	EndProductionYear   *int          `json:"end_production_year"`
+	DateCreate          time.Time     `json:"date_create"`
+	DateUpdate          time.Time     `json:"date_update"`
+	IDCarType           int           `json:"id_car_type"`
+	SerieName           string        `json:"serie_name"`
+	Generation          CarGeneration `json:"generation"`
+}
+
+type CarGeneration struct {
+	ID        int     `json:"id_car_generationn"`
+	Name      string  `json:"name"`
+	YearBegin string  `json:"year_begin"`
+	YearEnd   *string `json:"year_end"`
+	IDCarType int     `json:"id_car_type"`
 }
